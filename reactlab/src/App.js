176c3e86f535abc0acf8d19c3c66c9fc2af720d2
Chimezie Amazing <111098662+lagendary24@users.noreply.github.com>
2023-05-16 19:52:00 +0100

    controlled form

---
+++ reactlab/src/App.js
@@ -0,0 +1,132 @@
+import "./App.css";
+import { useState } from "react";
+import { validateEmail } from "./utils";
+
+const PasswordErrorMessage = () => {
+  return (
+    <p className="FieldError">Password should have at least 8 characters</p>
+  );
+};
+
+function App() {
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState({
+    value: "",
+    isTouched: false,
+  });
+  const [role, setRole] = useState("role");
+
+  const getIsFormValid = () => {
+    return (
+      firstName !== "" &&
+      validateEmail(email) &&
+      password.value.length >= 8 &&
+      (role === "individual" || role === "business")
+    );
+  };
+
+  const clearForm = () => {
+    setFirstName("");
+    setLastName("");
+    setEmail("");
+    setPassword({ value: "", isTouched: false });
+    setRole("role");
+  };
+
+  const handleFirstNameChange = (event) => {
+    setFirstName(event.target.value);
+  };
+
+  const handleLastNameChange = (event) => {
+    setLastName(event.target.value);
+  };
+
+  const handleEmailChange = (event) => {
+    setEmail(event.target.value);
+  };
+
+  const handlePasswordChange = (event) => {
+    setPassword({ value: event.target.value, isTouched: true });
+  };
+
+  const handleRoleChange = (event) => {
+    setRole(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    if (getIsFormValid()) {
+      alert("Account created!");
+      clearForm();
+    }
+  };
+
+  return (
+    <div className="App">
+      <form onSubmit={handleSubmit}>
+        <fieldset>
+          <h2>Sign Up</h2>
+          <div className="Field">
+            <label>
+              First name <sup>*</sup>
+            </label>
+            <input
+              placeholder="First name"
+              value={firstName}
+              onChange={handleFirstNameChange}
+            />
+          </div>
+          <div className="Field">
+            <label>Last name</label>
+            <input
+              placeholder="Last name"
+              value={lastName}
+              onChange={handleLastNameChange}
+            />
+          </div>
+          <div className="Field">
+            <label>
+              Email address <sup>*</sup>
+            </label>
+            <input
+              placeholder="Email address"
+              value={email}
+              onChange={handleEmailChange}
+            />
+          </div>
+          <div className="Field">
+            <label>
+              Password <sup>*</sup>
+            </label>
+            <input
+              type="password"
+              placeholder="Password"
+              value={password.value}
+              onChange={handlePasswordChange}
+            />
+            {password.isTouched && password.value.length < 8 && (
+              <PasswordErrorMessage />
+            )}
+          </div>
+          <div className="Field">
+            <label>
+              Role <sup>*</sup>
+            </label>
+            <select value={role} onChange={handleRoleChange}>
+              <option value="role">Role</option>
+              <option value="individual">Individual</option>
+              <option value="business">Business</option>
+            </select>
+          </div>
+          <button type="submit" disabled={!getIsFormValid()}>
+            Create account
+          </button>
+        </fieldset>
+      </form>
+    </div>
+  );
+}
+
+export default App;
